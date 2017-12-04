80b93f2a85d41c6b6eda33b3a17b95c90a1de695
Katherine Over <katherine.over@gmail.com>
2017-12-04 13:20:03 -0500

    add firebase authentication

--- src/App.js
+++ src/App.js
@@ -3,26 +3,34 @@ import './App.css';
 import firebase from './firebase.js';
 import { RoomList } from './components/RoomList.js';
 import { MessageList } from './components/MessageList.js';
+import { User } from './components/User.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {activeRoom: ""};
+    this.state = {activeRoom: "", user: null};
     this.activeRoom = this.activeRoom.bind(this);
+    this.setUser = this.setUser.bind(this);
   }
 
 activeRoom(room) {
   this.setState({ activeRoom: room })
 }
 
+setUser(user) {
+  this.setState({ user: user });
+}
+
   render() {
     const showMessages = this.state.activeRoom;
+
     return (
       <div>
         <h1>{this.state.activeRoom.title || "Select A Room"}</h1>
+        <User firebase={firebase} setUser={this.setUser} />
         <RoomList firebase={firebase} activeRoom={this.activeRoom} />
         { showMessages ?
-        (<MessageList firebase={firebase} activeRoom={this.state.activeRoom.key}/>)
+        (<MessageList firebase={firebase} activeRoom={this.state.activeRoom.key} />)
         : (null)
         }
       </div>
